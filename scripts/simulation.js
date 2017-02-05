ded23201100df119c625c401dec3487b949e5622
Abe Fetterman <abe@nomiku.com>
2017-02-04 17:24:32 -0800

    Use kad-encrypt

--- scripts/simulation.js
+++ scripts/simulation.js
@@ -9,9 +9,9 @@ var faker = require('faker');
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
-const spartacus = require('kad-spartacus');
+const encrypt = require('kad-encrypt');
 
-var NUM_NODES = 6;
+var NUM_NODES = 2;
 
 // Let simulation runners specify the number of nodes they would like to
 // create for the simulation.
@@ -34,7 +34,7 @@ var seed = [];
 // Create the number of nodes specified for the simulation and stick them into
 // an array so we can connect them to one another.
 while (created < NUM_NODES) {
-  let keypair = new spartacus.KeyPair();
+  let keypair = new encrypt.KeyPair();
   var node = new DHTNode({
     address: '127.0.0.1',
     port: port-created,
